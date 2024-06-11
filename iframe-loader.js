9ca90b32ad44d1e115f112d7913b9aa708033031
Ahmed-Ibrahim99 <82116411+Ahmed-Ibrahim99@users.noreply.github.com>
2024-06-11 17:42:58 +0300

    Update iframe-loader.js

--- iframe-loader.js
+++ iframe-loader.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const container = document.getElementById("payment-container");
     if (container) {
         const iframe = document.createElement("iframe");
-        iframe.src = "payment.html"; // The URL of the iframe content
+        iframe.src = "https://Ahmed-Ibrahim99.github.io/payment-iframe/payment.html"; // The URL of the iframe content
         iframe.width = "100%";
         iframe.height = "400px"; // Adjust the height as needed
         iframe.style.border = "none";
